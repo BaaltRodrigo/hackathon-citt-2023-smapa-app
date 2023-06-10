f34220b4eb8f11f5ad5b25e48a6f945353dbecd1
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2023-06-10 03:10:45 -0400

    Sample map

---
+++ src/components/SmapaMap.vue
@@ -0,0 +1,34 @@
+<template>
+  <l-map
+    ref="map"
+    v-model:zoom="zoom"
+    :center="center"
+    :useGlobalLeaflet="false"
+  >
+    <l-tile-layer :url="url" :name="name" layer-type="base" />
+  </l-map>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+
+export default {
+  name: "SmapaMap",
+  components: {
+    LMap,
+    LTileLayer,
+  },
+
+  data: () => ({
+    zoom: 15,
+    url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    name: "SMAPA-MAP",
+    center: [-33.5162105, -70.7525183, 15], // Maipu
+  }),
+
+  mounted() {
+    console.log("Mounted map");
+  },
+};
+</script>
