71b0569d26b51355a92f91107d7fea5a113e2a13
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2023-06-10 01:26:20 -0400

    Add firebase and store configuration

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from "vuex";
+// import AuthModule from "./auth";
+
+export default createStore({
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {
+    // auth: AuthModule,
+  },
+});
