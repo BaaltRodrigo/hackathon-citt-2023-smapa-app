ad78123b2bf2fc6eea1adf9e21943e774d35e7ae
BaaltRodrigo <r.antonio.regueiro@gmail.com>
2023-06-10 03:06:08 -0400

    Add sidebar

---
+++ src/components/TheSidebar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-navigation-drawer app permanent rail>
+    <v-list density="compact" nav>
+      <v-list-item
+        prepend-icon="mdi-view-dashboard"
+        value="dashboard"
+      ></v-list-item>
+
+      <v-list-item prepend-icon="mdi-forum" value="messages"></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "TheSidebar",
+
+  components: {
+    //
+  },
+
+  data: () => ({
+    //
+  }),
+
+  mounted() {
+    console.log("Mounted TheSidebar");
+  },
+};
+</script>
